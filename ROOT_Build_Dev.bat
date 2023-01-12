0a9ae5a1f16e96a340db62da1c9c1c314d2f597b
yanyoumo <yanyoumo@gmail.com>
2023-01-12 17:54:27 +0800

    initalCommit

---
+++ ROOT_Build_Dev.bat
@@ -0,0 +1,13 @@
+@echo off
+setlocal
+:PROMPT
+SET /P AREYOUSURE=Are you sure to pack and Upload Package to Steam(Y/[N])?
+IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
+
+@echo on
+
+ContentBuilder\builder\steamcmd.exe +login theArkitect_2022 vsPCRoKnZLeq}Av2 +run_app_build %~dp0\BuildScripts\root_app_build.vdf +quit
+PAUSE
+
+:END
+endlocal
